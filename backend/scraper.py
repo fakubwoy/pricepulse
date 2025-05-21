47102294cc02059ba6c66421cfea962a5dc576ae
fakubwoy <farhaan.khan2022@vitstudent.ac.in>
2025-05-21 19:48:45 +0530

    updated the dummy script in scraper.py

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,6 +1,6 @@
 def dummy_scrape_amazon(url: str):
     return {
-        "name": "Samsung Galaxy M14",
-        "price": 12999,
+        "name": "Samsung Galaxy M35",
+        "price": 18499,
         "image": "https://example.com/image.jpg"
     }
